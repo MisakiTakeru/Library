56b69ed95c436387fa3fb85a6798b31a1f957092
unknown <nardun@gmail.com>
2024-03-11 12:44:11 +0100

    added base non implemented handler functions

---
+++ Code/handler.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from singletonDatabaseConnect import SingletonDatabaseConnect as SDC
+import db_class
+from factory import Factory
+
+
+
+class Datahandler:
+    
+    
+    def return_book():
+        pass    
+        
+    
+    
+    
+    def borrow():
+        pass
+        
+
+
+    def reserve():
+        pass    
+        
