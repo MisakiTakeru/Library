7bac6a06245279de5ab175fcb8f5e642e3ddd70c
Frederik Mogensen <frode.moge@gmail.com>
2024-03-13 10:30:13 +0100

    handle reserve book

--- Code/handler.py
+++ Code/handler.py
@@ -3,6 +3,7 @@ from singletonDatabaseConnect import SingletonDatabaseConnect as SDC
 import db_class
 from factory import Factory
 from sqlalchemy import update
+from datetime import datetime
 
 
 class Datahandler:
@@ -11,7 +12,39 @@ class Datahandler:
         self.session = session 
         self.engine = engine
 
-    
+    def reserve_book(self, book_isbn, user_id):
+        user = self.session.query(db_class.User).filter_by(id = user_id).first()
+        if user is None:
+            raise ValueError(f"No user found with id {user_id}")
+
+        #get listof books with isbn
+        books = self.session.query(db_class.Book).filter_by(isbn = book_isbn).all()
+        if books is None:
+            raise ValueError(f"No book found with isbn {book_isbn}")
+        
+        #check if book is already reserved by user
+        if book_isbn in user.reserved:
+            raise ValueError(f"Book with isbn {book_isbn} is already reserved by user with id {user_id}")
+        
+        #user should borrow instead of reserve if book is available
+        for book in books:
+            if book.borrow_status == False:
+                raise ValueError(f"Book with isbn {book_isbn} is available for borrowing")
+        
+            user.reserved[book_isbn] = datetime.now()
+
+        try:
+            print(f"User {user.name} reserved book with isbn {book_isbn} successfully")
+            # Print user in a loop with all components
+            for component in user.__dict__.items():
+                print(component)
+            self.session.commit()
+            return True  
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            self.session.rollback()
+            return False 
+
     def return_book(self, book_id):
         book = self.session.query(db_class.Book).filter_by(id = book_id).first()
         if book is None:
@@ -34,15 +67,16 @@ class Datahandler:
         try:
             print(f"User {user.name} returned book with id {book_id} and title {book.title} successfully")
             self.session.commit()
-            return True  # Return True if commit is successful
+            return True  
         except Exception as e:
             print(f"An error occurred: {e}")
-            return False  # Return False if an error occurred
+            self.session.rollback()
+            return False  
         
     
     
     
-    def borrow(name, uid):
+    def borrow(self, name, uid):
         book = self.session.query(db_class.Book).filter_by(title = name).\
             filter_by(borrowed = False).first()
         if book == None:
