a102a66c11cbdedd608c279b95907074743fd5ec
FrodeMog <96060147+FrodeMog@users.noreply.github.com>
2024-03-13 20:24:25 +0100

    make table with book statuses of 3 bools, available, reserved, borrowed

--- Code/db_class.py
+++ Code/db_class.py
@@ -5,33 +5,27 @@ from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base() #Base class from sqlalchemy
 
-class Borrowed(Base):
-    __tablename__ = 'borrowed'
+class BookStatus(Base):
+    __tablename__ = 'book_status'
 
     id = Column(Integer, primary_key=True)
     timestamp = Column(String)
     user_id = Column(Integer, ForeignKey('users.id'))
     book_id = Column(Integer, ForeignKey('books.id'))
+    status_borrowed = Column(Boolean)
+    status_reserved = Column(Boolean)
+    status_available = Column(Boolean)
 
-    def __init__(self, timestamp, user_id, book_id):
+    user = relationship('User', back_populates='book_statuses')
+    book = relationship('Book', back_populates='book_statuses')
+
+    def __init__(self, timestamp, user_id, book_id, status_borrowed, status_reserved, status_available):
         self.timestamp = timestamp
         self.user_id = user_id
         self.book_id = book_id
-
-class Reserved(Base):
-    __tablename__ = 'reserved'
-
-    id = Column(Integer, primary_key=True)
-    isbn = Column(String)
-    timestamp = Column(String)
-    user_id = Column(Integer, ForeignKey('users.id'))
-    book_id = Column(Integer, ForeignKey('books.id'))
-
-    def __init__(self, isbn, timestamp, user_id, book_id):
-        self.isbn = isbn
-        self.timestamp = timestamp
-        self.user_id = user_id
-        self.book_id = book_id
+        self.status_borrowed = status_borrowed
+        self.status_reserved = status_reserved
+        self.status_available = status_available
 
 class Book(Base):
     __tablename__ = 'books'
@@ -41,16 +35,14 @@ class Book(Base):
     title = Column(String)
     author = Column(String)
     release_date = Column(String)
-    borrowed = relationship('Borrowed', backref='book')
-    reserved = relationship('Reserved', backref='book')
+    book_statuses = relationship('BookStatus', back_populates='book')
 
-    def __init__(self, isbn, title, author, release_date,  borrowed = None, reserved = None):
+    def __init__(self, isbn, title, author, release_date, book_statuses=None):
         self.isbn = isbn
         self.title = title
         self.author = author
         self.release_date = release_date
-        self.borrowed = borrowed if borrowed is not None else []
-        self.reserved = reserved if reserved is not None else []
+        self.book_statuses = book_statuses if book_statuses is not None else []
 
 class User(Base):
     __tablename__ = 'users'
@@ -59,15 +51,13 @@ class User(Base):
     name = Column(String)
     address = Column(String)
     email = Column(String)
-    borrowed = relationship('Borrowed', backref='user')
-    reserved = relationship('Reserved', backref='user')
+    book_statuses = relationship('BookStatus', back_populates='user')
 
-    def __init__(self, name, address, email, borrowed = None, reserved = None):
+    def __init__(self, name, address, email, book_statuses=None):
         self.name = name
         self.address = address
         self.email = email
-        self.borrowed = borrowed if borrowed is not None else []
-        self.reserved = reserved if reserved is not None else []
+        self.book_statuses = book_statuses if book_statuses is not None else []
 
 class Log:
     __tablename__ = 'logs'
@@ -83,4 +73,3 @@ class Log:
         self.func = func
         self.bid = bid
         self.uid = uid
-
